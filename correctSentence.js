885bb9b1b097a4b4ac85709c18ba7b5ce05094c1
neu3n0 <neu3n0.sH@gmail.com>
2022-10-05 13:16:08 +0300

    solution for correctSentence

--- correctSentence.js
+++ correctSentence.js
@@ -18,6 +18,15 @@ correctSentence("Greetings, friends.") == "Greetings, friends."
  */
 
 export default function correctSentence(text) {
-  // your solution goes here
+  /*
+  // if the input is number, it might be better to return the converted number to str and parse the number as str
+  if (typeof text == 'number') {
+    text = String(text);
+  }
+  else if (typeof text != 'string') return false;
+  */
+  if (typeof text != 'string') return false;
+  if (text == "") return ""; // probably its better
+  text = text.charAt(0).toUpperCase() + text.slice(1) + (text[text.length - 1] != '.' ? '.' : '');
   return text;
 }
