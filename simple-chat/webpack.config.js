762353285049f89ec290b716e7e48dbb3b94a33c
neu3n0 <neu3n0.sH@gmail.com>
2022-11-03 06:48:09 +0300

    almost ready

--- simple-chat/webpack.config.js
+++ simple-chat/webpack.config.js
@@ -66,7 +66,7 @@ module.exports = {
                         loader: 'file-loader',
                         options: {
                             name: '[name].[ext]',
-                            outputPath: 'img',
+                            outputPath: 'images',
                         },
                     },
                 ],
