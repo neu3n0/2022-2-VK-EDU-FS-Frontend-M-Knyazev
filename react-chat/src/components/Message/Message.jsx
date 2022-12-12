eb48c49ab77e84be41c7dc02164a1cff2d2deb3d
neu3n0 <neu3n0.sH@gmail.com>
2022-12-12 18:52:36 +0300

    fixes: add useEffect destructor for setInterval, add username for message

--- react-chat/src/components/Message/Message.jsx
+++ react-chat/src/components/Message/Message.jsx
@@ -5,26 +5,34 @@ import styles from './Message.module.scss'
 
 
 export default function Message(props) {
-    let st = 'flex-end'; 
+    let st = 'flex-end';
     if (props.message["author_username"] !== "neu3n0") {
         st = 'flex-start';
-    } 
+    }
 
     return (
         <div className={styles.message} style={{
             'justifyContent': st
         }}>
-            <div className={styles.messageWrap}>
-                <span className={styles.messageText}>
-                    {props.message['text']}
-                </span>
 
-                <div className={styles.messageMeta}>
-                    <div className={styles.messageStatus}>
-                        <DoneAllIcon className="done_all" style={{fontSize: '17px',  color: 'rgb(35, 182, 35)'}}/>
+            <div className={styles.messageWrap}>
+                <div className={styles.messageUp}>
+                    <div className={styles.messageUsername}>
+                    {props.message['author_username']}
                     </div>
-                    <div className={styles.messageTime}>
-                        {props.message['pub_date']}
+                </div>
+                <div className={styles.messageDown}>
+                    <span className={styles.messageText}>
+                        {props.message['text']}
+                    </span>
+
+                    <div className={styles.messageMeta}>
+                        <div className={styles.messageStatus}>
+                            <DoneAllIcon className="done_all" style={{ fontSize: '17px', color: 'rgb(35, 182, 35)' }} />
+                        </div>
+                        <div className={styles.messageTime}>
+                            {props.message['pub_date']}
+                        </div>
                     </div>
                 </div>
             </div>
