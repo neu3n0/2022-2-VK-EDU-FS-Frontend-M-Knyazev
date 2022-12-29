7ed23bdaf90192f802281326f5df395f61b4c1ef
neu3n0 <neu3n0.sH@gmail.com>
2022-12-30 00:31:53 +0300

    add curr lanhg

--- exam/src/components/TranslateForms/TranslateForms.jsx
+++ exam/src/components/TranslateForms/TranslateForms.jsx
@@ -45,7 +45,7 @@ export default function TranslateForms() {
     return (
         <div>
             <div>
-                
+                <p>Текущий язык: {lang}</p>
                 <select className={styles.lang} onClick={(e) => {setLang(e.target.value); translate(inp, setOut, e.target.value);}}>
                     {listLangs}
                 </select>
