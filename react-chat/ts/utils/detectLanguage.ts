515946a207434c3e6876a222ab5e6edbd2ac09b4
neu3n0 <neu3n0.sH@gmail.com>
2022-12-29 17:55:40 +0300

    almost work

---
+++ react-chat/ts/utils/detectLanguage.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { IAM_token, folderId } from './keys';
+
+export async function detectLanguage(text: string) {
+
+    const head: any = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + IAM_token,
+        }
+    };
+
+    const bodyParameters: any = {
+        text: text,
+        folderId: folderId,
+        languageCodeHints: [
+            "en", "ru"
+        ],
+    };
+
+    const data: any = await axios.post('https://translate.api.cloud.yandex.net/translate/v2/detect', bodyParameters, head);
+    // console.log(data.data.languageCode);
+    return data.data.languageCode;
+}
+
+detectLanguage('hello')
