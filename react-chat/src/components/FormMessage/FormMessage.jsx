4fd5da17fd484526642e0c9e006c2ccd4d403594
neu3n0 <neu3n0.sH@gmail.com>
2023-01-08 17:40:39 +0300

    part

--- react-chat/src/components/FormMessage/FormMessage.jsx
+++ react-chat/src/components/FormMessage/FormMessage.jsx
@@ -5,7 +5,7 @@ import styles from './FormMessage.module.scss'
 export default function FormMessage(props) {
 
     return (
-        <div className={styles.inputLine}>
+        <div className={styles.inputLine} onDragEnter={ props.handleDrag } onDragOver={ props.handleDrag } onDragLeave={ props.handleDrag } onDrop={ props.handleDrop }>
             <form className={styles.formMessage} onSubmit={props.handleSubmit}>
                 <input className="form-input" name="message-text" placeholder="Enter your message" type="text" value={props.val} onChange={(e) => {props.setVal(e.target.value)}} />
             </form>
